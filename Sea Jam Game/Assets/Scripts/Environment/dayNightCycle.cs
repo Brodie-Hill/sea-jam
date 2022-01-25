dd94f9d279994e65a4a95689e7902535838ab604
Brodie Hill <brodiehill79@gmail.com>
2022-01-26 00:48:50 +1300

    Started water V2

--- Sea Jam Game/Assets/Scripts/Environment/dayNightCycle.cs
+++ Sea Jam Game/Assets/Scripts/Environment/dayNightCycle.cs
@@ -7,6 +7,7 @@ public class dayNightCycle : MonoBehaviour
     [SerializeField] private Light sun = null;
     [SerializeField] private Vector3 rotationAxis = Vector3.up;
     [SerializeField] private float rotationSpeed = 0.25f;
+    private float timeAtLastEnvUpdate = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -14,11 +15,18 @@ public class dayNightCycle : MonoBehaviour
         rotationAxis.Normalize();
         sun.transform.rotation = Quaternion.LookRotation(rotationAxis, Vector3.up);
         sun.transform.Rotate(90, 0, 0, Space.Self);
+
+        timeAtLastEnvUpdate = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
         sun.transform.Rotate(rotationAxis * rotationSpeed * Time.deltaTime, Space.Self);
+        /*if (Time.time > timeAtLastEnvUpdate +1)
+        {
+            DynamicGI.UpdateEnvironment();
+            timeAtLastEnvUpdate++;
+        }*/
     }
 }
